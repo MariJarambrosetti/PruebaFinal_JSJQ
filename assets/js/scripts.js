5d07949d71ad6ad155f73a368b570451be4bbe77
mariana <mariana.j.ambrosetti@gmail.com>
2018-06-05 18:11:59 -0400

    clase likeable y rows para los tweets

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
 
-	var tweet = $('#comentarea').val();
+	
 
 	$('#myform').on('submit', function(event){
-		event.preventDefault();
-		$('.comments').append('<div class="comment">'+
+	event.preventDefault();
+		var text = $('#comentarea').val();
+		$('#comments').append(
+			'<div class="comment">'+
 			'<img src="assets/images/profilepic.jpg" alt="profile" class="commentimg img-responsive">'+
 			'<h3>Homero Jimeno Simpsons</h3>'+
-			tweet+
+			'<p>'+
+			text + 
+			'</p>'+
 			'<i class="fas fa-trash-alt basura"></i>'+
-			'<i class="fas fa-heart corazon"></i>'
-		)
+			'<i class="fas fa-heart corazon"></i>'+
+			'</div>')
+		$('#comentarea').val('');
+		$('#comentarea').focus();
+	});
 
+	$('.comments').on('click', '.corazon', function(event){
+		event.preventDefault();
+		event.stopPropagation();
+		$(this).toggleClass('corazon_likeable');
 	})
 
 
-})
+
+
+});
